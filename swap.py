d2c23e4623a07e8fbfda8a9abf2f16056118c1f1
Divya Sawant <86359301+DivyaRsawant@users.noreply.github.com>
2023-04-13 11:41:48 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,9 @@
+num1 = float(input("Enter num 1 :"))
+num2 = float(input("Enter num 2 :"))
+
+temp= num1
+num1 = num2
+num2 = temp
+
+print("the value of num1 after swapping is", num1)
+print("the value of num2 after swapping is", num2)
